7021f14a0e78f279c36ddb046eb0d3b871780f66
Nikita <razinkovnik@gmail.com>
2020-06-25 16:24:23 +0300

    convert and load data

--- primary_task.py
+++ primary_task.py
@@ -5,6 +5,7 @@ from transformers import BertTokenizer, AdamW, BertForNextSentencePrediction
 import torch
 from typing import List
 from args import TrainingArguments
+import pandas as pd
 
 
 def read_data(filename: str):
@@ -27,9 +28,9 @@ def json2data(item):
 
 
 def collate(data: List, tokenizer: BertTokenizer, block_size: int) -> Dict:
-    labels = [item['label'] for item in data]
-    questions = [item['question'] for item in data]
-    texts = [item['text'] for item in data]
+    questions = [item[0] for item in data]
+    texts = [item[1] for item in data]
+    labels = [item[2] for item in data]
     input_data = tokenizer.batch_encode_plus(list(zip(questions, texts)), max_length=block_size,
                                              truncation='only_second', pad_to_max_length=True, return_tensors="pt").to(
         args.device)
@@ -37,11 +38,21 @@ def collate(data: List, tokenizer: BertTokenizer, block_size: int) -> Dict:
     return input_data
 
 
-def load_data(filename: str, tokenizer: BertTokenizer, batch_size: int, args: TrainingArguments) -> DataLoader:
-    data = read_data(filename)
+def json2df(input_file: str, output_file: str):
+    data = read_data(input_file)
     items = [part for parts in [json2data(item) for item in data] for part in parts]
-    negative_items = [item for item in items if item['label'] == 1]
-    positive_items = [item for item in items if item['label'] == 0]
+    questions = [item['question'] for item in items]
+    texts = [item['text'] for item in items]
+    labels = [item['label'] for item in items]
+    df = pd.DataFrame({"questions": questions, "texts": texts, "labels": labels})
+    df.to_csv(output_file)
+
+
+def load_data(filename: str, tokenizer: BertTokenizer, batch_size: int, args: TrainingArguments) -> DataLoader:
+    df = pd.read_csv(filename)
+    items = list(zip(df.questions.to_list(), df.texts.to_list(), df.labels.to_list()))
+    negative_items = [item for item in items if item[2] == 1]
+    positive_items = [item for item in items if item[2] == 0]
     shuffle(negative_items)
     items = positive_items + negative_items[:len(positive_items)]
     return build_data_iterator(items, batch_size, lambda data: collate(data, tokenizer, args.block_size))
@@ -49,7 +60,7 @@ def load_data(filename: str, tokenizer: BertTokenizer, batch_size: int, args: Tr
 
 if __name__ == "__main__":
     args = setup()
-    # args.train_dataset = "dataset/tydiqa.jsonl"
-    # args.test_dataset = "dataset/tydiqa.jsonl"
+    # args.train_dataset = "dataset/primary_train.csv"
+    # args.test_dataset = "dataset/primary_test.csv"
     tokenizer, model, optimizer = init_model(BertForNextSentencePrediction, args)
     train(tokenizer, model, optimizer, load_data, args)
